a74b18675696b56fe450a230021401a77deee11c
PavanPadamata <pavanpadmata2002@gmail.com>
2025-10-12 21:04:36 +0530

    sm7325-common: Initial PixelOS Bringup!

--- common.mk
+++ common.mk
@@ -21,10 +21,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
-# Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
-
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
