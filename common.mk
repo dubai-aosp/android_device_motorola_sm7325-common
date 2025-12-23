244785bdf8c64caadd3e5861a9f0f6191390fb15
PavanPadamata <pavanpadmata2002@gmail.com>
2025-12-23 20:11:37 +0100

    sm7325-common: PixelOS Bringup!
    Change-Id: Ib04cedcb4ac7347793a1e3197f2c175965516d45

--- common.mk
+++ common.mk
@@ -23,7 +23,7 @@ $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-custom
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
