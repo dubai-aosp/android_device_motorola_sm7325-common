b25b5c120835659999ad3328c7d006f3bd410669
PavanPadamata <pavanpadmata2002@gmail.com>
2025-12-28 09:24:44 +0000

    fixup! sm7325-common: overlay: Show Turbo Charging instead of charging rapidly

--- common.mk
+++ common.mk
@@ -23,6 +23,7 @@ $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-custom
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
